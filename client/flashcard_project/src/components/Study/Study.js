760ea2cb9ec2993a57ef9e1e39e409ce98bc9cde
Sharon Brown <xSharonB030705x@gmail.com>
2023-04-12 15:53:25 -0400

    Third Page updates

--- client/flashcard_project/src/components/Study/Study.js
+++ client/flashcard_project/src/components/Study/Study.js
@@ -1,109 +1,22 @@
-import React, {useState, useEffect}  from 'react'
-import StopWatch from "./StopWatch"
-import Timer from './Timer'
-import axios from 'axios'
-import Quiz from "./Quiz"
-// import { DeckContext } from '../../Context'
+import React from 'react';
+import Card from "../Card/Card";
 
+function Study(props) {
+  const { cards } = props;
 
-
-function Study() {
-    const [getDecks, setGetDecks] = useState([])
-    const [selectedDeck, setSelectedDeck] = useState("")
-    const [cards, setCards] = useState([])
-    // const { getDecks } = useContext(DeckContext)
-
-    
-    
-    // const data = [
-
-    // {
-    //         question: "alskjdflakjsdf;lakjsdf", 
-    //     answer: "askdhfkasjfhlkajsdf", 
-    //     _id: "1"
-    // }, 
-    // {
-    //         question: "kajshdfkljahsdklfjhalksdhf", 
-    //         answer: "fkajfdkjashdlf", 
-    //         _id: "2"    
-    // }
-    // ]
-
-
-        useEffect(() => {
-            axios.get('/decks')
-            .then(res => {
-                setGetDecks(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }, [])
-
-        useEffect(() => {
-            if(selectedDeck){
-                axios.get(`/decks/${setSelectedDeck}/cards`)
-                .then(res =>{
-                    setCards(res.data)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-            }
-        }, [selectedDeck])
-
-        function handleDeckChange(e){
-            setSelectedDeck(e.target.value)
-        }
-        function handleQuiz(){
-            console.log("button clicked")
-            return(
-                <Quiz />
-          
-            )
-        }
-
-    return (
-    <div className='study'>
-        <div className='timers-container'>
-            <StopWatch className= 'stopwatch-component'/>
-            <Timer className = "timer-component"/>
-        </div> 
-        
-        <div className='button-and-list-container'>
-        <div className='quiz-study-button-container'>
-        <button onClick = {handleQuiz} className='quiz-button'>Quiz</button>  
-        <button className='study-button'>Study</button>
-        </div>
-        <div className='question-list'>
-            <select value={selectedDeck} onChange={handleDeckChange}>
-                <option value = "">Select a deck</option>
-                {getDecks.map(deck => (
-                    <option key = {deck.id} value = {deck.id}>{deck.name}</option>
-                ))}
-            </select>
-            {cards.length > 0 && (
-                <div>
-                
-                    <h3>{cards[0].deckName}</h3>
-                    <ul>
-                        {cards.map(card => (
-                            <li key = {card.id}>
-                               <p> {card.question}</p>
-                               <p>{card.answer}</p>
-                            </li>
-                        ))}
-                    </ul>
-                    
-                </div>
-                
-            )}
-        </div>  
-        </div>
+  return (
+    <div>
+      <h3>Question List</h3>
+      <ul>
+        {cards.map((card) => (
+          <li key={card.id}>
+            <Card card={card} showAnswer={true} />
+          </li>
+        ))}
+      </ul>
     </div>
-    
-  )
+  );
 }
 
-export default Study
+export default Study;
 
